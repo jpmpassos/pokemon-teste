15ec5675539a8b79acb38a1c3cc1ebbf964f39c0
jpmpassos <jp1_1_1_1@yahoo.com.br>
2020-05-21 23:21:09 -0300

    Acertando calculo de cores(type)

--- src/app/model/detalhes.model.ts
+++ src/app/model/detalhes.model.ts
@@ -1,5 +1,6 @@
 import { BaralhoModel } from './baralho.model';
 import { isNullOrUndefined } from 'util';
+import { CardModel } from './card.model';
 
 export class DetalhesModel {
     baralho: BaralhoModel;
@@ -11,17 +12,53 @@ export class DetalhesModel {
     ) {
         this.baralho = baralho;
         this.processarDetalhes();
+        this.processarCores();
     }
 
     private processarDetalhes() {
-        this.countTreinadores = 1;
-        this.baralho.cards.forEach((card) => {
+        this.countCores = 0;
+        this.countPokemon = 0;
+        this.countTreinadores = 0;
+        for (let index = 0; index < this.baralho.cards.length; index++) {
+            let card = this.baralho.cards[index];
             if (card.supertype == "Pokémon")
                 this.countPokemon++;
             if (card.supertype == "Trainer")
                 this.countTreinadores++;
-            if (!isNullOrUndefined(card.types) && card.types.length > 0)
-                this.countCores++;
-        });
+        }
     }
+
+    private processarCores() {
+        let list = this.baralho.cards.sort(this.compararType);
+        let card = this.baralho.cards[0];
+        if (!isNullOrUndefined(card.types)) this.countCores++;
+        for (let index = 1; index < list.length; index++) {
+            let card2 = list[index];
+            let result = this.compararType(card2, card);
+            if (result != 0) {
+                if (!isNullOrUndefined(card2.types)){
+                    this.countCores++;
+                    card = card2;
+                } 
+            }
+        }
+    }
+
+    compararType(c1: CardModel, c2: CardModel): number {
+        if (c1.types == c2.types) return 0;
+        if (isNullOrUndefined(c1.types)) return 1;
+        if (isNullOrUndefined(c2.types)) return -1;
+        if (c1.types.length == c2.types.length) {
+            for (let index = 0; index < c1.types.length; index++) {
+                if (c1.types[index] > c2.types[index]) {
+                    return 1;
+                } else if (c1.types[index] < c2.types[index]) {
+                    return -1;
+                }
+            }
+            return 0
+        }
+    }
+
+
 }
